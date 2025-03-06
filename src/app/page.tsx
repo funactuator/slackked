7b32f7ccc9f08ea36ec3424d47c9bdbe1cfc04ea
funactuator <kuldeep.msvm@gmail.com>
2025-03-06 18:00:58 +0530

    get all workspaces query setup, store setup for workspace modal, jotai addition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
 "use client";
 
+import { useEffect, useMemo } from "react";
+
 import { UserButton } from "@/features/auth/components/user-button";
 
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces();
+  const [open, setOpen] = useCreateWorkspaceModal();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+    else if (!!workspaceId) {
+      console.log("Redirect to workspace");
+    } else if (!open) {
+      console.log("Open Workspace Creation Modal");
+      setOpen(!open);
+    }
+  }, [workspaceId, isLoading, open, setOpen]);
+
   return (
     <div>
       <UserButton />
