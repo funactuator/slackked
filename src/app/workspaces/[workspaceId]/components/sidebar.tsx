7f5c042b4738b925addf06f500412e5ee714e085
funactuator <kuldeep.msvm@gmail.com>
2025-03-07 23:51:06 +0530

    workspace id page and user button minor fix

--- src/app/workspaces/[workspaceId]/components/sidebar.tsx
+++ src/app/workspaces/[workspaceId]/components/sidebar.tsx
@@ -5,7 +5,7 @@ export const Sidebar = () => {
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-1">
       <div><WorkspaceSwitcher/></div>
-      <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
+      <div className="flex flex-col items-center justify-center gap-y-1 mt-auto mb-1">
         <UserButton />
       </div>
     </aside>
