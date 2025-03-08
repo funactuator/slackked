c3b36c724fca82df2650390132afbfd2ecb71513
funactuator <kuldeep.msvm@gmail.com>
2025-03-09 02:33:27 +0530

    created confirmation box for modal

---
+++ src/hooks/use-confirm.tsx
@@ -0,0 +1,58 @@
+import { JSX, useState } from "react";
+
+import {
+  Dialog,
+  DialogHeader,
+  DialogFooter,
+  DialogContent,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { Button } from "@/components/ui/button";
+
+export const UseConfirm = (
+  title: string,
+  message: string
+): [() => JSX.Element, () => Promise<unknown>] => {
+  const [promise, setPromise] = useState<{
+    resolve: (value: boolean) => void;
+  } | null>(null);
+
+  const confirm = () => {
+    return new Promise((resolve, _) => {
+      setPromise({ resolve });
+    });
+  };
+
+  const handleClose = () => {
+    setPromise(null);
+  };
+
+  const handleCancel = () => {
+    promise?.resolve(false);
+    handleClose();
+  };
+
+  const handleConfirm = () => {
+    promise?.resolve(true);
+    handleClose();
+  };
+
+  const ConfirmationDialog = () => (
+    <Dialog open={promise !== null}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{message}</DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="pt-2">
+          <Button onClick={handleCancel} variant="outline">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirm}>Confirm</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+  return [ConfirmationDialog, confirm];
+};
