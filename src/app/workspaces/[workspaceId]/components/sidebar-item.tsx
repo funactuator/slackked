b73022c284a0bd3e76c59c6c2c31e6f8f307d7ff
funactuator <kuldeep.msvm@gmail.com>
2025-03-09 03:17:58 +0530

    implemented sidebar item with variant style

---
+++ src/app/workspaces/[workspaceId]/components/sidebar-item.tsx
@@ -0,0 +1,52 @@
+import { LucideIcon } from "lucide-react";
+import { IconType } from "react-icons/lib";
+import Link from "next/link";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { cn } from "@/lib/utils";
+
+const SidebarItemVariants = cva(
+  "flex justify-start items-center gap-1.5 font-normal h-7 px-[18px] text-sm overflow-hidden",
+  {
+    variants: {
+      variant: {
+        default: "text-[#F9EDFFCC]",
+        active: "text-[#481349] bg-white/90 hover:bg-white/90",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+interface SidebarItemProps {
+  label: string;
+  icon: LucideIcon | IconType;
+  id: string;
+  variant?: VariantProps<typeof SidebarItemVariants>["variant"];
+}
+
+export const SidebarItem = ({
+  label,
+  icon: Icon,
+  id,
+  variant,
+}: SidebarItemProps) => {
+  const workspaceId = useWorkspaceId();
+  return (
+    <Button
+      className={cn(SidebarItemVariants({ variant }))}
+      variant="transparent"
+      size="sm"
+      asChild
+    >
+      <Link href={`/workspaces/${workspaceId}/channels/${id}`}>
+        <Icon className="size-3.5 mr-1 shrink-0" />
+        <span className="text-sm truncate">{label}</span>
+      </Link>
+    </Button>
+  );
+};
