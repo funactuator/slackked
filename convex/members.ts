4285b41864002f2128945bee64e0657c80a5e8fc
funactuator <kuldeep.msvm@gmail.com>
2025-03-12 23:24:44 +0530

    added get members api

--- convex/members.ts
+++ convex/members.ts
@@ -1,7 +1,12 @@
 import { v } from "convex/values";
 
-import { query } from "./_generated/server";
+import { query, QueryCtx } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+import { Id } from "./_generated/dataModel";
+
+const populateUser = (ctx: QueryCtx, id: Id<'users'>) => {
+  return ctx.db.get(id);
+}
 
 /**
  * Get Member Details of current logged in user in a given workspace;
@@ -22,5 +27,42 @@ export const current = query({
       .unique();
     if (!member) return null;
     return member;
+  },
+});
+
+export const get = query({
+  args: { workspaceId: v.id("workspaces") },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) return [];
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) return [ ];
+
+    const data = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id", (q) =>
+        q.eq("workspaceId", args.workspaceId)
+      )
+      .collect();
+
+    const isLoading = data === undefined;
+
+    const members = [];
+    for(const member of data){
+      const user = await populateUser(ctx, userId);
+      if(user){
+        members.push({
+          ...member, ...user
+        })
+      }
+    }
+    return { members, isLoading };
   },
 });
