35bd0e9661f1f5caf7f8613d63db7e000a4c42e3
funactuator <kuldeep.msvm@gmail.com>
2025-03-14 02:09:43 +0530

    minor changes

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,28 +1,76 @@
 "use client";
-
-import Image from "next/image";
 import VerificationInput from "react-verification-input";
+import Link from "next/link";
+import Image from "next/image";
+import { Loader } from "lucide-react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+
+import { Button } from "@/components/ui/button";
+import { useJoin } from "@/features/workspaces/api/use-join";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+import { cn } from "@/lib/utils";
 
 const JoinPage = () => {
+  const router = useRouter();
+  const workspaceId = useWorkspaceId();
+  const { mutate, isPending } = useJoin();
+  const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
+
+  const handleComplete = (value) => {
+    mutate({workspaceId, joinCode: value}, {
+      onSuccess: (id) => {
+        router.replace(`/workspaces/${id}`)
+        toast.success("Workspace Joined!")
+      },
+      onError: () => {
+        toast.error("Failed to join workspace")
+      }
+    })
+  }
+
+  if(isLoading){
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground"/>
+      </div>
+    )
+  }
+
   return (
-    <div className="h-full flex flex-col gap-y-4 items-center justify-center bg-white px-8 rounded-lg shadow-md">
-      <Image className="-mt-20" src="/logo.svg" alt="logo" width={60} height={60} />
+    <div className="h-full flex flex-col gap-y-6 items-center justify-center bg-white px-8 rounded-lg shadow-md">
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        width={60}
+        height={60}
+      />
       <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
         <div className="flex flex-col gap-y-2 items-center justify-center">
-          <h1 className="text-2xl font-bold">Join Workspace</h1>
+          <h1 className="text-2xl font-bold">Join {data?.name}</h1>
           <p className="text-md text-muted-foreground">
-            Enter the workspace code to join{" "}
+            Enter the workspace code to join
           </p>
         </div>
         <VerificationInput
+          autoFocus
+          length={6}
           classNames={{
-            container: "verification-container",
-            character: "verification-character",
-            characterInactive: "verification-character-inactive",
-            characterSelected: "verification-character-selected",
-            characterFilled: "verification-character-filled",
+            container: cn('!flex !gap-x-2', isPending && 'opacity-50 cursor-not-allowed'),
+            character: "!uppercase !h-auto !rounded-xs !border !border-gray-300 !flex !items-center !justify-center !text-lg !font-medium !text-gray-500",
+            characterInactive: "!bg-muted",
+            characterSelected: "!bg-white !text-black",
+            characterFilled: "!bg-white !text-black",
           }}
+          onComplete={handleComplete}
         />
+      </div>
+      <div className="flex mt-2 gap-x-4">
+        <Button variant="outline" size="lg" asChild>
+          <Link href="/">Back to home </Link>
+        </Button>
       </div>
     </div>
   );
