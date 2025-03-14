21164768f94ee5be8fbe4f60184f7af351b0280e
funactuator <kuldeep.msvm@gmail.com>
2025-03-15 00:38:26 +0530

    added quill for editor and basic styling

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,9 +1,37 @@
+import Quill, {type QuillOptions} from 'quill'
+import "quill/dist/quill.snow.css"
+import { useEffect, useRef } from 'react';
+
 const Editor = () => {
-    return (
-        <div>
-            Editor
-        </div>
-    )
-}
+
+  const containerRef = useRef<HTMLDivElement>(null);  
+
+  useEffect(() => {
+    if(!containerRef.current)return;
+
+    const container = containerRef.current;
+    const editorContainer = container.appendChild(
+        container.ownerDocument.createElement("div")
+    );
+
+    const options: QuillOptions = {
+        theme: 'snow',
+    }
+
+    new Quill(editorContainer, options);
+    return () => {
+        if(container){
+            container.innerHTML = ""
+        }    
+    }
+  }, [])
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:bg-slate-300 focus-within:shadow-sm transition bg-white">
+        <div ref={containerRef}>Editor</div>
+      </div>
+    </div>
+  );
+};
 
 export default Editor;
