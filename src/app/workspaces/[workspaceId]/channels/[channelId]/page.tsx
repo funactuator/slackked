bc82a0ffd15c8859f1960f6eb7521eac180679d3
funactuator <kuldeep.msvm@gmail.com>
2025-03-15 00:21:57 +0530

    started with chat input component

--- src/app/workspaces/[workspaceId]/channels/[channelId]/page.tsx
+++ src/app/workspaces/[workspaceId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { useChannelId } from "@/hooks/use-channel-id";
 
 import { useGetChannel } from "@/features/channels/api/use-get-channel";
 import { Header } from "./components/header";
+import { ChatInput } from "./components/chat-input";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
@@ -32,6 +33,8 @@ const ChannelIdPage = () => {
 
   return <div className="flex flex-col h-full">
     <Header title = {channel.name} />
+    <div className="flex-1"/>
+    <ChatInput/>
   </div>;
 };
 
