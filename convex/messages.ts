b511acb0cec62bb6cd5736711067aaab43d89673
funactuator <kuldeep.msvm@gmail.com>
2025-03-15 20:43:09 +0530

    added create message api

---
+++ convex/messages.ts
@@ -0,0 +1,55 @@
+import { getAuthUserId } from "@convex-dev/auth/server";
+import { mutation, QueryCtx } from "./_generated/server";
+import { v } from "convex/values";
+import { Id } from "./_generated/dataModel";
+
+const getMemmber = async (
+  ctx: QueryCtx,
+  workspaceId: Id<"workspaces">,
+  userId: Id<"users">
+) => {
+  return ctx.db
+    .query("members")
+    .withIndex("by_workspace_id_user_id", (q) =>
+      q.eq("workspaceId", workspaceId).eq("userId", userId)
+    )
+    .unique();
+};
+
+export const create = mutation({
+  args: {
+    body: v.string(),
+    image: v.optional(v.id("_storage")),
+    workspaceId: v.id("workspaces"),
+    channelId: v.optional(v.id("channels")),
+    parentMessageId: v.optional(v.id("messages")),
+    // add conversationId
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await getMemmber(ctx, args.workspaceId, userId);
+
+    if(!member){
+        throw new Error("Unauthorized")
+    }
+
+    // Handle conversation Id
+
+    const messageId = await ctx.db.insert("messages", {
+        memberId: member._id,
+        channelId: args.channelId,
+        workspaceId: args.workspaceId,
+        parentMessagedId: args.parentMessageId,
+        body: args.body,
+        image: args.image,
+        updatedAt: Date.now()
+    })
+
+    return messageId;
+  },
+});
